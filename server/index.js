5c4785e72d272ab5049999c8d52710a9845e5533
Linden Chiu <hchiu87@gmail.com>
2020-08-08 00:02:01 -0700

    clean up, set up passport

--- server/index.js
+++ server/index.js
@@ -9,9 +9,33 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 console.log(accountSid, authToken, twilioPhoneNumber)
+console.log()
 const client = require('twilio')(accountSid, authToken);
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const session = require('express-session');
+
+// config express-session
+const sess = {
+  secret: 'CHANGE THIS TO A RANDOM SECRET',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
+
+app.use(session(sess));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
