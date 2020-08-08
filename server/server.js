7a258d4b543a9cd963772302cd9a4e92fedca303
AWang <KHLover1234@gmail.com>
2020-08-07 17:11:44 -0700

    Fixed all changes from /sms to /api/sms

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//To see it work, run "node server.js" and go to http://localhost:3000/sms
+//To see it work, run "node server.js" and go to http://localhost:3000/api/sms
 
 //Message Alexa on discord for the accountSid and authToken
 const accountSid = "";
@@ -37,10 +37,10 @@ app.get('/api/sms', (req, res) => {
 
 //This only works if you have twilio CLI installed (need a twilio account)
 // To get this functionality, you need to run this server
-// And then say this command: twilio phone-numbers:update "+15017122661" --sms-url="http://localhost:3000/sms"
+// And then say this command: twilio phone-numbers:update "+15017122661" --sms-url="http://localhost:3000/api/sms"
 // And update the phone number above into the one that you're using
 // And then finally reply to the number that the twilio message came from
-app.post('/sms', (req, res) => {
+app.post('/api/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
   twiml.message('The Robots are coming! Head for the hills!');
